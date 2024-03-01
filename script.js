d37f2ed7ea103edfed4cddec71530205eaa20c3c
Ed_Mugo <edwinmurgoh@gmail.com>
2024-03-01 10:29:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const accordion = document.querySelectorAll('.accordion-button');
+
+accordion.forEach( btn => {
+    btn.addEventListener('click', function(){
+        this.classList.toggle('active')
+        const accordionContent = this.nextElementSibling
+        const plusIcon = this.querySelector('.plus-icon')
+        const minusIcon = this.querySelector('.minus-icon')
+
+        if(accordionContent.style.maxHeight){
+            accordionContent.style.maxHeight= null;
+            plusIcon.style.display= 'block'
+            minusIcon.style.display = 'none'
+        } else{
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+            plusIcon.style.display = 'none'
+            minusIcon.style.display = 'block'
+        }
+    })  
+});
